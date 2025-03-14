12852fbd1b21d9b185a3d20bd0ab60d38a9e0698
Maria Góral <mariagoral@MacBook-Air-Maria.local>
2025-03-14 19:51:36 +0100

    Modyfikacja 3

--- MojaAplikacja/Program.cs
+++ MojaAplikacja/Program.cs
@@ -7,6 +7,23 @@ class Program
         Console.Write("Podaj swoje imię: ");
         string name = Console.ReadLine();
         Console.WriteLine("Witaj, " + name + "!");
+
+        Console.Write("Podaj pierwszą liczbę: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Podaj drugą liczbę: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int sum = num1 + num2;
+
+        Console.WriteLine("Suma tych liczb wynosi: " + sum);
+
+        if (sum > 10)
+        {
+            Console.WriteLine("Suma jest większa niż 10!");
+        }
+        else
+        {
+            Console.WriteLine("Suma jest mniejsza lub równa 10.");
+        }
     }
 }
 
