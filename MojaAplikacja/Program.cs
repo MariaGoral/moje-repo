180fb315d6a1eabe4d8b25ec40cdc729c23ae707
Maria Góral <mariagoral@MacBook-Air-Maria.local>
2025-03-14 19:50:38 +0100

    Modyfikacja 2

--- MojaAplikacja/Program.cs
+++ MojaAplikacja/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        // Modyfikacja 1: Dodanie drugiego komunikatu
-        Console.WriteLine("This is my first .NET application.");
+        Console.Write("Podaj swoje imię: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Witaj, " + name + "!");
     }
 }
 
